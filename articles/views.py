654d1f771f9d8f8e0e4df38b7d1ba64077e109ad
Nicole Bahri <bahrinicole1@gmail.com>
2023-12-02 11:32:33 -0400

    edit

--- articles/views.py
+++ articles/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView 
+from django.views.generic.edit import UpdateView
+
 
 from .models import Article
 
@@ -12,3 +14,11 @@ class ArticleListView(ListView):
 class ArticleDetailView(DetailView):
     model = Article
     template_name = "article_detail.html"
+
+class ArticleUpdateView(UpdateView):
+    model = Article
+    fields= (
+        'title',
+        'body',
+    )
+    template_name = "article_edit.html"
